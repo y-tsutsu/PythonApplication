6ee4c233de0b6e7edb6afd2d7e803aa69fe34e45
y-tsutsumi <tsutsu0928@gmail.com>
2013-10-23 21:34:19 +0900

    ファイル一覧取得．

---
+++ PythonApplication/FileList.py
@@ -0,0 +1,35 @@
+﻿# coding: utf-8
+
+import os, os.path
+import glob
+
+path = "C:\\Python27\\"
+
+print("+--------------------+")
+print(u"ファイル＋ディレクトリ一覧")
+print("+--------------------+")
+
+files = os.listdir(path)
+
+for file in files:
+    print(file)
+
+# ---------------------------------
+
+print("+--------------------+")
+print(u"指定パス＋ファイル一覧")
+print("+--------------------+")
+
+files = glob.glob(path)
+
+for file in files:
+    print(file)
+
+# ---------------------------------
+
+print("+--------------------+")
+print(u"ファイルとサブディレクトリ一覧")
+print("+--------------------+")
+
+for r, ds, fs in os.walk(path):
+    print(r, ds, fs)
